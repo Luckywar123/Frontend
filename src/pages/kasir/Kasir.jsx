ce512192b1d5c39a76dcf19396c81654d0bb82ba
Andre Luckyta Firdaus <andreluckytafirdaus2001@gmail.com>
2023-08-16 15:38:07 +0700

    bug fixing button kasir

--- src/pages/kasir/Kasir.jsx
+++ src/pages/kasir/Kasir.jsx
@@ -105,6 +105,7 @@ export const Kasir = () => {
   };
   
   
+  
   const fetchLatestIdTransaksi = async () => {
     try {
       const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/kasir`);
